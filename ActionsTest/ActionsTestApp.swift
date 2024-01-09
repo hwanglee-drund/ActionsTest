dcb5fdbacf392c7d7c5ceba29aae83a34123e552
hwanglee-drund <hwanglee@drund.com>
2024-01-09 10:24:55 -0500

    initial

---
+++ ActionsTest/ActionsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionsTestApp.swift
+//  ActionsTest
+//
+//  Created by DrundDev Three on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ActionsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
