29ad9063c03d8ecc3ab1c5a40cbfb2856fd13cfe
hwanglee-drund <hwanglee@drund.com>
2024-01-09 12:21:52 -0500

    update tests

--- ActionsTestTests/ActionsTestTests.swift
+++ ActionsTestTests/ActionsTestTests.swift
@@ -24,6 +24,9 @@ final class ActionsTestTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
+        XCTAssertTrue(0 == 0)
+        XCTAssertTrue(0 == 1)
     }
 
     func testPerformanceExample() throws {
